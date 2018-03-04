76b241af0ead251d47909c6c9b5e40eaf88b6487
Elton Law <elton.law@scm.ca>
2018-03-04 02:07:40 -0500

    add overlapping permutations algorithm

--- diehard/overlapping_permutations.py
+++ diehard/overlapping_permutations.py
@@ -1,9 +1,11 @@
 """ overlapping_permutations.py """
+import collections
+import numpy as np
 from diehard.utils import preprocess
 from diehard.utils import chunker
 
 @preprocess
-def overlapping_permutations(arr, cons=5):
+def overlapping_permutations(arr, consecutive=5):
     """ Analyze sequences of n consecutive real numbers. All possible
     orderings should occur with statistically equal probability
 
@@ -15,3 +17,11 @@ def overlapping_permutations(arr, cons=5):
     RETURNS
     -------
     """
+    # n_permutations = np.math.factorial(consecutive)
+    chunks = chunker(arr, consecutive, overlapping=True, complete=True)
+    chunks_np = np.array(list(chunks))
+    orderings = np.argsort(chunks_np)
+    unique_counter = collections.Counter((map(tuple, orderings)))
+
+    # TODO: Compute variance analysis and randomness likelihood
+    return unique_counter
